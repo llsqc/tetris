0376425e2c2ffa2e63c1f45059e9abaca9ee6eda
isolation <23796978@qq.com>
2025-03-03 00:02:30 +0800

    feat: 输入线程

--- tetris/GameSceneUpdate/GameScene.cs
+++ tetris/GameSceneUpdate/GameScene.cs
@@ -5,58 +5,85 @@ public class GameScene : ISceneUpdate
     Map map;
     BlockManagement blockManagement;
 
+    Thread inputThread;
+
     public GameScene()
     {
         map = new Map();
         blockManagement = new BlockManagement();
+
+        inputThread = new Thread(CheckInputThread)
+        {
+            IsBackground = true
+        };
+        inputThread.Start();
+    }
+
+    private void CheckInputThread()
+    {
+        while (true)
+        {
+            if (Console.KeyAvailable)
+            {
+                lock (blockManagement)
+                {
+                    switch (Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.LeftArrow:
+                            if (blockManagement.CanChange(E_ChangeType.Left, map))
+                            {
+                                blockManagement.Change(E_ChangeType.Left);
+                            }
+
+                            break;
+                        case ConsoleKey.RightArrow:
+                            if (blockManagement.CanChange(E_ChangeType.Right, map))
+                            {
+                                blockManagement.Change(E_ChangeType.Right);
+                            }
+
+                            break;
+                        case ConsoleKey.A:
+                            if (blockManagement.CanMoveLeftRight(E_ChangeType.Left, map))
+                            {
+                                blockManagement.MoveLeftRight(E_ChangeType.Left);
+                            }
+
+                            break;
+                        case ConsoleKey.D:
+                            if (blockManagement.CanMoveLeftRight(E_ChangeType.Right, map))
+                            {
+                                blockManagement.MoveLeftRight(E_ChangeType.Right);
+                            }
+
+                            break;
+                        case ConsoleKey.S:
+                            if (blockManagement.CanAutoMove(map))
+                            {
+                                blockManagement.AutoMove();
+                            }
+
+                            break;
+                    }
+                }
+            }
+        }
     }
 
     public void Update()
     {
-        map.Draw();
-
-        blockManagement.Draw();
-
-        if (blockManagement.CanAutoMove(map))
+        lock (blockManagement)
         {
-            blockManagement.AutoMove();
+            map.Draw();
+
+            blockManagement.Draw();
+
+            if (blockManagement.CanAutoMove(map))
+            {
+                blockManagement.AutoMove();
+            }
         }
 
         Thread.Sleep(200);
-
-        if (Console.KeyAvailable)
-        {
-            switch (Console.ReadKey(true).Key)
-            {
-                case ConsoleKey.LeftArrow:
-                    if (blockManagement.CanChange(E_ChangeType.Left, map))
-                    {
-                        blockManagement.Change(E_ChangeType.Left);
-                    }
-
-                    break;
-                case ConsoleKey.RightArrow:
-                    if (blockManagement.CanChange(E_ChangeType.Right, map))
-                    {
-                        blockManagement.Change(E_ChangeType.Right);
-                    }
-
-                    break;
-                case ConsoleKey.A:
-                    if (blockManagement.CanMoveLeftRight(E_ChangeType.Left, map))
-                    {
-                        blockManagement.MoveLeftRight(E_ChangeType.Left);
-                    }
-
-                    break;
-                case ConsoleKey.D:
-                    if (blockManagement.CanMoveLeftRight(E_ChangeType.Right, map))
-                    {
-                        blockManagement.MoveLeftRight(E_ChangeType.Right);
-                    }
-
-                    break;
-            }
-        }
     }
 }
