ed655b7b3f2a7ce13fb2e47941906289f0eb7706
isolation <23796978@qq.com>
2025-03-04 17:11:44 +0800

    feat: 实现方块消除

--- tetris/Map/Map.cs
+++ tetris/Map/Map.cs
@@ -1,4 +1,6 @@
-﻿namespace tetris;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace tetris;
 
 public class Map : IDraw
 {
@@ -8,14 +10,22 @@ public class Map : IDraw
     public int mapW;
     public int mapH;
 
+    private int[] recordInfo;
+
     public Map()
     {
         mapH = Game.height - 6;
+        mapW = 0;
+
+        recordInfo = new int[mapH];
 
         for (int i = 0; i < Game.width; i += 2)
         {
             walls.Add(new DrawObject(E_DrawType.Wall, i, mapH));
+            ++mapW;
         }
+
+        mapW -= 2;
 
         for (int i = 0; i < mapH; i++)
         {
@@ -37,12 +47,62 @@ public class Map : IDraw
         }
     }
 
+    public void ClearDraw()
+    {
+        for (int i = 0; i < dynamicWalls.Count; i++)
+        {
+            dynamicWalls[i].ClearDrawObject();
+        }
+    }
+
     public void AddWalls(List<DrawObject> walls)
     {
         for (int i = 0; i < walls.Count; i++)
         {
             walls[i].ChangeType(E_DrawType.Wall);
             dynamicWalls.Add(walls[i]);
+
+            recordInfo[mapH - 1 - walls[i].pos.y]++;
+        }
+
+        ClearDraw();
+        CheckClear();
+        Draw();
+    }
+
+    public void CheckClear()
+    {
+        List<DrawObject> delList = new List<DrawObject>();
+        for (int i = 0; i < recordInfo.Length; i++)
+        {
+            if (recordInfo[i] == mapW)
+            {
+                for (int j = 0; j < dynamicWalls.Count; j++)
+                {
+                    if (i == mapH - dynamicWalls[j].pos.y - 1)
+                    {
+                        delList.Add(dynamicWalls[j]);
+                    }
+                    else if (mapH - 1 - dynamicWalls[j].pos.y > i)
+                    {
+                        ++dynamicWalls[j].pos.y;
+                    }
+                }
+
+                for (int j = 0; j < delList.Count; j++)
+                {
+                    dynamicWalls.Remove(delList[j]);
+                }
+
+                for (int j = i; j < recordInfo.Length - 1; j++)
+                {
+                    recordInfo[j] = recordInfo[j + 1];
+                }
+
+                recordInfo[^1] = 0;
+                CheckClear();
+                break;
+            }
         }
     }
 }
